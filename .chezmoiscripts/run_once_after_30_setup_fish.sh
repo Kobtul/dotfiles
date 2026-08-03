1d774cc460cdd0d195630e8efdf8f1296a6e8f07
Jan Pokorný <JenomPokorny@gmail.com>
2026-08-03 15:10:26 +0200

    fix: fish setup

--- .chezmoiscripts/run_once_after_30_setup_fish.sh
+++ .chezmoiscripts/run_once_after_30_setup_fish.sh
@@ -3,6 +3,5 @@ set -euxo pipefail
 
 echo
 echo "### SETUP FISH ###"
-source ~/.profile
 grep -qxF "$HOME/.local/share/mise/shims/fish" /etc/shells || echo "$HOME/.local/share/mise/shims/fish" | sudo tee -a /etc/shells
 chsh -s "$HOME/.local/share/mise/shims/fish"
