99e3466052c9519999431b8832c7fd78856562de
David Kubesa <d.kubesa@gmail.com>
2026-08-25 17:45:53 +0200

    Handle unset profile variables during setup

--- .chezmoiscripts/run_once_after_30_setup_fish.sh
+++ .chezmoiscripts/run_once_after_30_setup_fish.sh
@@ -3,7 +3,9 @@ set -euxo pipefail
 
 echo
 echo "### SETUP FISH ###"
+set +u
 source ~/.profile
+set -u
 fish_path="$(command -v fish)"
 grep -qxF "$fish_path" /etc/shells || echo "$fish_path" | sudo tee -a /etc/shells
 chsh -s "$fish_path"
